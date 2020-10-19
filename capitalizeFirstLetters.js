5d3dc0cc7d8883661bceec7db5a182e168f47b3a
Erberack <brieucquertier73@gmail.com>
2020-10-19 09:40:53 +0200

    I wrote tests after assertions

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,5 +1,20 @@
-const assert = require('assert');
+function capitalFirstLetters(str) {
+  let words = str.split(' ');
 
+  let array = [];
+
+  words.forEach((word) => {
+    let firstLetter = word.charAt(0).toUpperCase();
+
+    let replacement = word.replace(word.charAt(0), firstLetter);
+
+    array.push(replacement);
+    console.log(array.join(' '));
+  });
+  return array.join(' ');
+}
+
+const assert = require('assert');
 assert.strictEqual(
   capitalFirstLetters('string with words'),
   'String With Words'
